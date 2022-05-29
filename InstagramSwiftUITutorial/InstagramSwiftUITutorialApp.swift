8653b4dda133e617a62e0a5e90fdc94ea8ac15af
ivancity <shieldm@hotmail.com>
2022-05-29 13:44:30 -0500

    Initial Commit

---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Ivan on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
