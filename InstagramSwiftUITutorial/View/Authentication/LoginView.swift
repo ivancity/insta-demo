9a2383de1b66a1633a4e3081c549c1f41fa5c8fc
ivancity <shieldm@hotmail.com>
2022-05-30 15:30:59 -0500

    building the loginView plus some custom text fields with customized colors for text placeholders.

--- InstagramSwiftUITutorial/View/Authentication/LoginView.swift
+++ InstagramSwiftUITutorial/View/Authentication/LoginView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -19,8 +23,92 @@ struct LoginView: View {
                         .scaledToFit()
                         .frame(width: 220, height: 100)
                     
+                    VStack(spacing: 20) {
+                        CustomTextField(
+                            text: $email,
+                            placeholder: Text("Email"),
+                            imageName: "envelope")
+                        .padding()
+                        .background(
+                            Color(.init(white: 1, alpha: 0.15))
+                        )
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                        
+                        CustomSecureField(
+                            text: $password,
+                            placeholder: Text("Password")
+                        )
+                        .padding()
+                        .background(
+                            Color(.init(white: 1, alpha: 0.15))
+                        )
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(
+                            action: {
+                                
+                            }
+                        ) {
+                            Text("Forgot Password?")
+                                .font(
+                                    .system(
+                                        size: 13,
+                                        weight: .semibold
+                                    )
+                                )
+                                .foregroundColor(.white)
+                                .padding(.top)
+                                .padding(.trailing, 28)
+                        }
+                        
+                    }
+                    
+                    Button(
+                        action: {
+                            
+                        }
+                    ) {
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 360, height: 50)
+                            .background(
+                                Color(
+                                    #colorLiteral(red: 1, green: 0.2527923882, blue: 1, alpha: 1)
+                                )
+                            )
+                            .clipShape(Capsule())
+                            .padding()
+                    }
+                    
                     Spacer()
                     
+                    Button(
+                        action: {
+                            
+                        }
+                    ) {
+                        HStack {
+                            Text("Dont have an account?")
+                                .font(
+                                    .system(size: 14)
+                                )
+                            Text("Sign Up")
+                                .font(
+                                    .system(size: 14, weight: .semibold)
+                                )
+                        }
+                        .foregroundColor(.white)
+                    }
+                    .padding(.bottom, 30)
                 }
             }
             
